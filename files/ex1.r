a8fb9410788a1b30407d0d543ed258303fd715c5
Matheus <mathew.mfranco@gmail.com>
2019-09-09 22:15:44 -0300

    Update ex1 with a second resolution

--- files/ex1.r
+++ files/ex1.r
@@ -5,6 +5,12 @@ S = P[is.na(P$Filhos), ]
 write.csv2(S,"suspeitos.csv",row.names = FALSE)
 
 # The file "honestos.csv" besides suspects data, also needs married employees witch have more than 15 minimum salary month
+
+#1º way
 P = P[!is.na(P$Filhos),]
 P = P[ !(P$Est.civil == "Casado" & P$Salario > 15),]
+
+#2º and much cleaner way
+P = P[ !((P$Est.civil == "Casado" & P$Salario > 15) | is.na(P$Filhos)), ]
+
 write.csv2(P,"honestos.csv",row.names = FALSE)
